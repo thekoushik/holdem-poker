3bdb84f173721c4b459481ae2dd7092c5532a88b
Tai <49883536+ctnava@users.noreply.github.com>
2022-09-22 04:32:31 -0500

    verbose testing (#20)

--- example/test-game1.js
+++ example/test-game1.js
@@ -6,39 +6,50 @@ dealt face up (the "River")and the the fourth and final betting round. At the Sh
 best five-card hand they can make using any five cards from the two pocket cards and the five community cards
 (or Board Cards).
 */
-var {Game} = require('..');
+var { Game } = require('..');
 //initialize with 2 players, each having 100 unit money, and initial bet is 10 unit
-var game=new Game([ 100, 100 ], 10);
+var game = new Game([100, 100], 10);
 //a demo gameplay is shown bellow
-console.log('Players',game.getState().players.map(function(m){return m.hand}))
-//round 1
+
+console.log('\nround 1 - no cards dealt (ante up)');
+console.log(
+  'Players',
+  game.getState().players.map(function (m) {
+    return m.hand;
+  })
+);
+console.log('Table', game.getState().communityCards);
 game.startRound();
-game.bet(0);//for player 1
-game.raise(1,20);//for player 2
-game.call(0)
-game.endRound()
-console.log('Table',game.getState().communityCards);
-//round 2
+game.bet(0); //for player 1
+game.raise(1, 20); //for player 2
+game.call(0);
+game.endRound();
+
+console.log('\nround 2 - 3 cards dealt (flop)');
+console.log('Table', game.getState().communityCards);
 game.startRound();
-game.check(0);//for player 1
-game.check(1);//for player 2
-game.endRound()
-console.log('Table',game.getState().communityCards);
-//round 3
+game.check(0); //for player 1
+game.check(1); //for player 2
+game.endRound();
+
+console.log('\nround 3 - 4 cards dealt');
+console.log('Table', game.getState().communityCards);
 game.startRound();
-game.raise(0,50);//for player 1
-game.call(1);//for player 2
-game.endRound()
-console.log('Table',game.getState().communityCards);
-//round 4
+game.raise(0, 50); //for player 1
+game.call(1); //for player 2
+game.endRound();
+
+console.log('\nround 4 - 5 cards dealt (river)');
+console.log('Table', game.getState().communityCards);
 game.startRound();
-game.call(0);//for player 1
-game.call(1);//for player 2
-game.endRound()
-//end game
-var result=game.checkResult();
-if(result.type=='win'){
-    console.log('Player'+(result.index+1)+' won with '+result.name);
-}else{
-    console.log('Draw');
+game.call(0); //for player 1
+game.call(1); //for player 2
+game.endRound();
+
+console.log('\nend game');
+var result = game.checkResult();
+if (result.type == 'win') {
+  console.log('Player' + (result.index + 1) + ' won with ' + result.name);
+} else {
+  console.log('Draw');
 }
